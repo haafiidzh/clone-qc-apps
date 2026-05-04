3724896da91ba67c37d3eb39a62c6f5d62134fbe
haafiidzh <hafid.kusuma45@gmail.com>
2026-05-04 23:24:24 +0700

    feat: add translation

---
+++ lang/en/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // Validation Error Messages
+    'Email Required' => 'The email field is required.',
+    'Email Email' => 'The email must be a valid email address.',
+    'Password Required' => 'The password field is required.',
+    'Password Min' => 'The password must be at least :min characters long.',
+];
---
+++ lang/id/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    // Validation Error Messages
+    'Email Required' => 'Email harus diisi.',
+    'Email Email' => 'Email harus valid.',
+    'Password Required' => 'Password harus diisi.',
+    'Password Min' => 'Password harus minimal :min karakter.',
+];
